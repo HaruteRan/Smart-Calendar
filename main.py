d208a6eed6b6ec6d021b098821686ca36b3fa2c8
HaruteRan <46661336+HaruteRan@users.noreply.github.com>
2019-02-28 23:53:15 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,82 @@
+from time import time
+from kivy.app import App
+from os.path import dirname, join
+from kivy.lang import Builder
+from kivy.properties import NumericProperty, StringProperty, BooleanProperty,\
+    ListProperty
+from kivy.animation import Animation
+from kivy.uix.screenmanager import Screen
+from kivy.uix.button import Button
+
+
+class DemoScreen(Screen):
+    fullscreen = BooleanProperty(False)
+
+    def add_widget(self, *args):
+        if 'content' in self.ids:
+            return self.ids.content.add_widget(*args)
+        return super(DemoScreen, self).add_widget(*args)
+
+
+class DemoApp(App):
+    index = NumericProperty(-1)
+    current_title = StringProperty()
+    time = NumericProperty(0)
+    show_sourcecode = BooleanProperty(False)
+    sourcecode = StringProperty()
+    screen_names = ListProperty([])
+    hierarchy = ListProperty([])
+
+    def build(self):
+        self.title = "RCOS demo app"
+        #Clock.schedule_interval(self._update_clock)
+        #return Button(text='RCOS')
+
+        self.screens = {}
+        self.available_screens = ["Calendar", "Input"]
+        self.screen_names = self.available_screens
+        cudir = dirname(__file__)
+        self.available_screens = [join(cudir, 'data', 'screens', '{}.kv'.format(fn).lower()) for fn in self.available_screens]
+        self.go_next_screen()
+
+    def _update_clock(self):
+        self.time = time()
+
+    def go_next_screen(self):
+        self.index = (self.index + 1) % len(self.available_screens)
+        screen = self.load_screen(self.index)
+        sm = self.root.ids.sm
+        sm.switch_to(screen, direction='left')
+        self.current_title = screen.name
+        self.update_sourcecode()
+
+    def go_previous_screen(self):
+        self.index = (self.index - 1) % len(self.available_screens)
+        screen = self.load_screen(self.index)
+        sm = self.root.ids.sm
+        sm.switch_to(screen, direction='right')
+        self.current_title = screen.name
+        self.update_sourcecode()
+
+    def load_screen(self, index):
+        if index in self.screens:
+            return self.screens[index]
+        screen = Builder.load_file(self.available_screens[index])
+        self.screens[index] = screen
+        return screen
+
+    def update_sourcecode(self):
+        if not self.show_sourcecode:
+            self.root.ids.sourcecode.focus = False
+            return
+        self.root.ids.sourcecode.text = self.read_sourcecode()
+        self.root.ids.sv.scroll_y = 1
+
+    def read_sourcecode(self):
+        fn = self.available_screens[self.index]
+        with open(fn) as fd:
+            return fd.read()
+
+
+if __name__ == "__main__":
+    DemoApp().run()
